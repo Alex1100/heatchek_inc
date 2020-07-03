a9c511a612b4306b49ec1cb1ac50673a06c5f1c7
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-07-03 13:45:03 -0700

    add basic auth to api

--- database/employee_db/queries/index.js
+++ database/employee_db/queries/index.js
@@ -1,8 +1,5 @@
 const employeeQueries = require('./employees.queries');
 
 module.exports = {
-  userExistsSQL: employeeQueries.userExistsSQL,
-  updateLastLogin: employeeQueries.updateLastLogin,
-  getUserByEmailSQL: employeeQueries.getUserByEmailSQL,
-  createUserSQL: employeeQueries.createUserSQL,
+  ...employeeQueries,
 };
