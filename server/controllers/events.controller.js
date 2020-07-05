82cd8e19f5ce173060c394f8121c27a08246b030
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-07-04 22:23:38 -0700

    make it better

--- server/controllers/events.controller.js
+++ server/controllers/events.controller.js
@@ -1,6 +1,6 @@
 const addEvent = async (req, res) => {
   try {
-    console.log('REQ AND RES ARE: ', req, '\n\n\n\n', res);
+    console.log('REQ BODY: ', req.body, req.params);
   } catch (e) {
     console.log('E IS: ', e);
   }
