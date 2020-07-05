433037856d45eb6a89757ca5619ae0d09c3749fd
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-07-04 22:11:16 -0700

    make it better

--- services/calendly/webhooks.js
+++ services/calendly/webhooks.js
@@ -5,7 +5,7 @@ const subscribeToWebhooks = async (req, res) => {
     const subscribedResponse = await axios.post(
       'https://calendly.com/api/v1/hooks',
       {
-        url: 'http://localhost:5000/api/add-event',
+        url: process.env.NODE_ENV !== "DEV" ? 'https://heatchek-inc.herokuapp.com/api/add-event' : 'http://localhost:5000/api/add-event',
         events: ["invitee.created", "invitee.cancelled"],
       },
       {
