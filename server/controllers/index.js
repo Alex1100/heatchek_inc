bf7ba29755ffc8a02b49968097865c65af41b178
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-07-05 19:45:02 -0700

    add

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,11 @@
-const auth = require('./employees.controller');
+const employees = require('./employees.controller');
+const customers = require('./customers.controller');
 const events = require('./events.controller');
 const employeeJobs = require('./employeeJobs.controller');
 const customerEvents = require('./customerEvents.controller');
-const customers = require('./customers.controller');
 
 module.exports = {
-  ...auth,
+  ...employees,
   ...events,
   ...employeeJobs,
   ...customers,
