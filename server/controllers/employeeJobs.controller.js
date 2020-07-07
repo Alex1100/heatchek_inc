3bc593bd8d24552d7eea50d3d5b329cfebe7a506
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-07-06 21:42:50 -0700

    add changes to employeeJobs controller

--- server/controllers/employeeJobs.controller.js
+++ server/controllers/employeeJobs.controller.js
@@ -17,7 +17,7 @@ const employeeJobList = async (req, res) => {
     }
     console.log('SQL QUERY IS: ', getEmployeeJobsSQL({employeeId}));
     const employeesJobs = await employeeDBClient.query(getEmployeeJobsSQL({employeeId}));
-    console.log('EMPLOYEE JOBS ARE: ', employeesJobs);
+    console.log('EMPLOYEE JOBS ARE: ', employeesJobs.rows);
     res.status(200).send({
       jobs: employeeJobs ? employeesJobs.rows : [],
     });
