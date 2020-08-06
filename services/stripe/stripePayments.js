fc8200e19a8c8943f9071402aff2c5c03873e443
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-08-05 17:15:04 -0700

    add pay to stripe service

---
+++ services/stripe/stripePayments.js
@@ -0,0 +1,61 @@
+const stripeLib = require('stripe');
+let stripe;
+
+if (process.env.NODE_ENV !== 'prod') {
+  stripe = stripeLib(process.env.TEST_STRIPE_API_SECRET);
+} else {
+  stripe = stripeLib(process.env.PROD_STRIPE_API_SECRET);
+}
+
+function generateResponse(response, intent) {
+  console.log('RESPONSE IS: ', response);
+  console.log('INTENT IS: ', intent);
+  if (intent.status === 'succeeded') {
+    // Handle post-payment fulfillment
+    return response.send({ success: true });
+  } else if (intent.status === 'requires_action') {
+    // Tell the client to handle the action
+    return response.send({
+      requiresAction: true,
+      clientSecret: intent.client_secret
+    });
+  } else {
+    // Any other status would be unexpected, so error
+    return response.status(500).send({error: 'Unexpected status ' + intent.status});
+  }
+}
+
+const pay = async (request, response) => {
+  try {
+    let intent;
+    if (request.body.payment_method_id) {
+      // Create the PaymentIntent
+      intent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: 'usd',
+        confirm: true,
+        payment_method: request.body.payment_method_id,
+        confirmation_method: 'manual',
+        use_stripe_sdk: true
+      });
+    } else if (request.body.payment_intent_id) {
+      intent = await stripe.paymentIntents.confirm(
+        request.body.payment_intent_id
+      );
+    }
+    // Send the response to the client
+    return generateResponse(response, intent);
+  }  catch (e) {
+    if (e.type === 'StripeCardError') {
+      // Display error on client
+      return response.send({ error: e.message });
+    } else {
+      // Something else happened
+      return response.status(500).send({ error: e.type });
+    }
+  }
+};
+
+module.exports = {
+  pay,
+}
