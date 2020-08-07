9902adf06353704372405875483451c898196012
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-08-07 15:31:35 -0700

    add overlappingDates query

--- database/employee_db/queries/customerEvents.queries.js
+++ database/employee_db/queries/customerEvents.queries.js
@@ -2,7 +2,7 @@ const createCustomerEventSQL = ({
   customer_id,
   event_id,
 }) => `
-  INSERT INTO (event_id, customer_id)
+  INSERT INTO events (event_id, customer_id)
   VALUES('${event_id}', '${customer_id}')
   RETURNING *
 `;
