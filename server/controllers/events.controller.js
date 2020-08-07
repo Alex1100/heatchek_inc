6b6af524e0d2b30167f9c8497a0739cde0325200
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-08-07 15:04:58 -0700

    add overlappingDates query

--- server/controllers/events.controller.js
+++ server/controllers/events.controller.js
@@ -93,7 +93,7 @@ const addEvent = async (req, res) => {
     res.status(201).send({customerEvent: customerEvent.rows[0] });
   } catch (e) {
     console.log('E IS: ', e);
-    res.setStatus(403);
+    res.status(403);
     res.send({error: e});
   }
 };
