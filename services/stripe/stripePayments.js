a12e22da78ed48e1a11cff9eeff23fb671168a4e
Alexander Aleksanyan <alexanderaleksanyan@Alexanders-MacBook-Pro.local>
2020-08-15 16:50:09 -0700

    add a dummy  payment with a live card

--- services/stripe/stripePayments.js
+++ services/stripe/stripePayments.js
@@ -39,8 +39,8 @@ const pay = async (request, response) => {
       customer_id,
       event_id,
     } = request.body;
-    console.log('CUSTOMER ID AND EVENT ID: ', customer_id, event_id, getCustomerEventSQL({customer_id, event_id}));
-    const customerEventData = await employeeDBClient.query(getCustomerEventSQL({customer_id, event_id}));
+    console.log('CUSTOMER ID AND EVENT ID: ', customer_id, event_id, getCustomerEventSQL({customerId: customer_id, event_id}));
+    const customerEventData = await employeeDBClient.query(getCustomerEventSQL({customerId: customer_id, event_id}));
     // console.log('CUSTOMER IS: ', customerData.rows[0]);
     if (!customerEventData) {
       throw new Error('Unable to make a payment for a customer that is not in our system.')
